3d4b078c04970e19564b2909f63b23ac2752de1d
Marek Rudnicki <marekrud@gmail.com>
2012-01-29 23:24:53 +0100

    auto: marek@orange

--- nrn/__init__.py
+++ nrn/__init__.py
@@ -2,8 +2,6 @@ from __future__ import division
 
 __author__ = "Marek Rudnicki"
 
-import numpy as np
-from collections import namedtuple
 import os
 
 import neuron
@@ -13,9 +11,3 @@ lib_dir = os.path.dirname(__file__)
 neuron.load_mechanisms(lib_dir)
 
 from gbc import GBC_Point
-
-anf_type = [('typ', 'S3'),
-            ('cf', float),
-            ('spikes', np.ndarray)]
-
-
