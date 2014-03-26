98514c2c31145c9128986a19c332b4b0e0661d49
Marek Rudnicki <marekrud@gmail.com>
2014-03-26 15:45:57 +0100

    brian model cleanup + gbc brian demo

--- anf.py
+++ anf.py
@@ -33,57 +33,57 @@ make_anfs = make_anf_group
 
 
 
-class ANFs(object):
-    def __init__(self, anf_trains):
+# class ANFs(object):
+#     def __init__(self, anf_trains):
 
-        self.meta = anf_trains.drop('spikes', axis=1)
-
-
-        ### cfs
-        self.cfs = self.meta['cf']
+#         self.meta = anf_trains.drop('spikes', axis=1)
 
 
-        ### anf_type
-        self.neuron_types = self.meta['type']
+#         ### cfs
+#         self.cfs = self.meta['cf']
 
 
-        ### spikes
-        times = []
-        indices = []
-        for i,spikes in enumerate(anf_trains['spikes']):
-            times.append( spikes )
-            indices.append( np.ones(len(spikes)) * i )
+#         ### anf_type
+#         self.neuron_types = self.meta['type']
 
 
-        indices = np.concatenate( indices )
-        times = np.concatenate( times ) * second
-
-        self.group = brian.SpikeGeneratorGroup(
-            len(anf_trains),
-            spiketimes=(indices, times)
-        )
-
-
-        self.brian_objects = [self.group]
+#         ### spikes
+#         times = []
+#         indices = []
+#         for i,spikes in enumerate(anf_trains['spikes']):
+#             times.append( spikes )
+#             indices.append( np.ones(len(spikes)) * i )
 
 
+#         indices = np.concatenate( indices )
+#         times = np.concatenate( times ) * second
 
-def main():
-    import cochlea
+#         self.group = brian.SpikeGeneratorGroup(
+#             len(anf_trains),
+#             spiketimes=(indices, times)
+#         )
 
-    fs = 100e3
-    t = np.arange(0, 0.1, 1/fs)
-    s = np.sin(2 * np.pi * t * 1000)
-    s = pycat.set_dbspl(s, 30)
 
-    ear = cochlea.Zilany2009((3,2,1), cf=(80, 8000, 10))
-    anf_raw = ear.run(s, fs)
-
-    print anf_raw.dtype
-
-    anfs = ANFs(anf_raw)
+#         self.brian_objects = [self.group]
 
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     import cochlea
+
+#     fs = 100e3
+#     t = np.arange(0, 0.1, 1/fs)
+#     s = np.sin(2 * np.pi * t * 1000)
+#     s = pycat.set_dbspl(s, 30)
+
+#     ear = cochlea.Zilany2009((3,2,1), cf=(80, 8000, 10))
+#     anf_raw = ear.run(s, fs)
+
+#     print anf_raw.dtype
+
+#     anfs = ANFs(anf_raw)
+
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ demos/nrn_gbc_demo.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Marek Rudnicki"
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import cochlear_nucleus.nrn as cn
+
+
+
+def main():
+    cn.set_celsius(37)
+
+    gbc = cn.GBC_Point(
+        convergence=(2,1,1),
+        cf=1000,
+        endbulb_class='tonic',
+        record_voltages=True
+    )
+
+
+    gbc.set_endbulb_weights(
+        (0.06, 0.005, 0.003)
+    )
+
+
+    anf_trains = pd.DataFrame([
+        {'spikes':np.array([10,20])*1e-3, 'type':'hsr', 'cf':1000, 'duration':100e-3},
+        {'spikes':np.array([30,40])*1e-3, 'type':'hsr', 'cf':1000, 'duration':100e-3},
+        {'spikes':np.array([50,60])*1e-3, 'type':'hsr', 'cf':3333, 'duration':100e-3},
+        {'spikes':np.array([70,80])*1e-3, 'type':'msr', 'cf':1000, 'duration':100e-3},
+        {'spikes':np.array([80,90])*1e-3, 'type':'msr', 'cf':2222, 'duration':100e-3},
+        {'spikes':np.array([60,50])*1e-3, 'type':'lsr', 'cf':1000, 'duration':100e-3}
+    ])
+
+
+    print("ANFs before")
+    print(anf_trains)
+
+
+    gbc.load_anf_trains(anf_trains)
+
+
+    cn.run(
+        duration=100e-3,
+        objects=[gbc]
+    )
+
+
+    print()
+    print("ANFs after")
+    print(anf_trains)
+
+
+    print()
+    print("GBC spikes")
+    print(gbc.get_trains())
+
+
+    voltages = gbc.get_voltages()
+    plt.plot(voltages)
+    plt.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
