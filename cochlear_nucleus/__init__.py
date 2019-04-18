bd45ceffd1eec646c3e54cbee2db4d91a3253862
mr <>
2019-04-18 18:11:59 +0200

    import citing

--- cochlear_nucleus/__init__.py
+++ cochlear_nucleus/__init__.py
@@ -6,7 +6,29 @@
 from __future__ import division, print_function, absolute_import
 from __future__ import unicode_literals
 
-__author__ = "Marek Rudnicki"
-__copyright__ = "Copyright 2015, Marek Rudnicki"
-__license__ = "GPLv3+"
-__version__ = "1.4"
+import sys
+
+__version__ = "2"
+
+
+_citation = """Rudnicki M. and Hemmert W. (2017).
+High entrainment constrains synaptic depression levels of an in vivo
+globular bushy cell model.
+Frontiers in Computational Neuroscience, Frontiers Media SA, 2017, 11, pp. 1-11.
+doi:10.3389/fncom.2017.00016
+https://www.frontiersin.org/articles/10.3389/fncom.2017.00016/full"""
+
+_citation_width = 80
+
+print(
+    " Please cite ".center(_citation_width, '='),
+    file=sys.stderr
+)
+print(
+    _citation,
+    file=sys.stderr
+)
+print(
+    " Thank you! ".center(_citation_width, '='),
+    file=sys.stderr
+)
